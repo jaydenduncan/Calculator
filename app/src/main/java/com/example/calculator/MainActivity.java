afeb58ce49cf8cc0be02a41f70d10284562265df
Jayden <jduncan9@stu.jsu.edu>
2023-02-22 23:59:41 -0600

    Initial commit (extended branch)

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.calculator.databinding.ActivityMainBinding;
 
@@ -18,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        class CalculatorClickHandler implements View.OnClickListener{
+
+            @Override
+            public void onClick(View view){
+                String tag = ((Button) view).getTag().toString();
+                Toast toast = Toast.makeText(binding.getRoot().getContext(), tag, Toast.LENGTH_SHORT);
+                toast.show();
+                // INSERT EVENT HANDLING CODE HERE
+            }
+
+        }
     }
 }
