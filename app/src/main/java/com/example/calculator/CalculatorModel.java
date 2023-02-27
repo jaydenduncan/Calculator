91cf4f50c74378488c961b5f85ff6760e7c278b0
Jayden <jduncan9@stu.jsu.edu>
2023-02-27 01:14:11 -0600

    OP_SELECTED, RHS, RESULT state

--- app/src/main/java/com/example/calculator/CalculatorModel.java
+++ app/src/main/java/com/example/calculator/CalculatorModel.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import android.util.Log;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalculatorModel extends AbstractModel {
 
@@ -14,9 +15,10 @@ public class CalculatorModel extends AbstractModel {
 
     private BigDecimal lhs, rhs;
 
-    private String operator;
+    private Operator operator;
 
-    private boolean decimal;
+    private String result;
+
 
     public CalculatorModel(){
 
@@ -25,7 +27,6 @@ public class CalculatorModel extends AbstractModel {
         lhs = new BigDecimal(0);
         rhs = new BigDecimal(0);
         operator = null;
-        decimal = false;
 
     }
 
@@ -53,6 +54,11 @@ public class CalculatorModel extends AbstractModel {
 
     public void setScreen(String tag){
 
+        String oldText = screen.toString();
+        String newText;
+        long tempLong = 0;
+        double tempDouble = 0.0;
+
         switch(tag){
             case "btn1": case "btn2": case "btn3": case "btn4": case "btn5": case "btn6":
             case "btn7": case "btn8": case "btn9": case "btn0":
@@ -60,55 +66,178 @@ public class CalculatorModel extends AbstractModel {
                     // change state to lhs
                     changeState(CalculatorState.LHS);
 
-                    // append proper number to the screen
-                    setKey(tag.charAt(3));
+                    if(!(screen.toString().contains(".")) && (screen.toString().length() > 1)
+                            && (screen.toString().substring(0, 2).equals("-0"))){
+                        clearScreen();
+                        setKey('-');
+                        setKey(tag.charAt(3));
+                    }
+                    else{
+                        // clear the screen
+                        clearScreen();
+
+                        // append proper number to the screen
+                        setKey(tag.charAt(3));
+                    }
                 }
-                else if(state == CalculatorState.LHS){
-                    setKey(tag.charAt(3));
+                else if((state == CalculatorState.LHS) || (state == CalculatorState.RHS)){
+
+                    if(!(screen.toString().contains(".")) && (screen.toString().charAt(0) == '0')){
+                        clearScreen();
+                        setKey(tag.charAt(3));
+                    }
+                    else if(!(screen.toString().contains(".")) && (screen.toString().length() > 1)
+                            && (screen.toString().substring(0, 2).equals("-0"))){
+                        clearScreen();
+                        setKey('-');
+                        setKey(tag.charAt(3));
+                    }
+                    else{
+                        setKey(tag.charAt(3));
+                    }
                 }
                 else if(state == CalculatorState.OP_SELECTED){
                     changeState(CalculatorState.RHS);
+                    clearScreen();
+                    setKey(tag.charAt(3));
                 }
                 else{
                     changeState(CalculatorState.LHS);
+                    clearScreen();
+                    setKey(tag.charAt(3));
                 }
+
                 break;
+
             case "btnC":
                 // change state to clear
                 changeState(CalculatorState.CLEAR);
 
                 // clear the screen
-                screen.delete(0, screen.length());
+                clearScreen();
 
-                // place 0 as placeholder
-                String oldText = screen.toString();
-                String newText = "0";
-                firePropertyChange(CalculatorController.SCREEN_PROPERTY, oldText, newText);
                 break;
+
             case "btnDec":
                 // place decimal in input if allowed
-                if(!(screen.toString().contains("."))) {
+                if(state == CalculatorState.CLEAR){
+                    changeState(CalculatorState.LHS);
+
                     setKey('.');
                 }
-                break;
-            case "btnPlusMin":
-                String oldNumText = screen.toString();
-                String newNumText = "";
+                else if((state == CalculatorState.LHS) || (state == CalculatorState.RHS)){
+                    if (!(screen.toString().contains("."))) {
 
-                // toggle positive and negative input
-                if(oldNumText.contains("-")){
-                    newNumText = oldNumText.substring(1);
+                        setKey('.');
+                    }
+                }
+                else if(state == CalculatorState.OP_SELECTED){
+                    changeState(CalculatorState.RHS);
+                    clearScreen();
+                    setKey('0');
+                    setKey('.');
                 }
                 else{
-                    newNumText = screen.insert(0, "-").toString();
+                    changeState(CalculatorState.LHS);
+                    setKey('0');
+                    setKey('.');
                 }
 
-                // replace StringBuilder buffer
-                screen.delete(0, screen.length());
-                screen.append(newNumText);
 
-                firePropertyChange(CalculatorController.SCREEN_PROPERTY, oldNumText, newNumText);
                 break;
+
+            case "btnPlusMin":
+
+                if(state == CalculatorState.OP_SELECTED){
+                    changeState(CalculatorState.RHS);
+                    clearScreen();
+                    setKey('-');
+                    setKey('0');
+                }
+                else if(state == CalculatorState.RESULT){
+
+                    changeState(CalculatorState.LHS);
+
+                    if(result.contains("-")){
+                        firePropertyChange(CalculatorController.SCREEN_PROPERTY, result, result.substring(1));
+                        result = result.substring(1);
+                        lhs = new BigDecimal(result);
+                    }
+                    else{
+                        firePropertyChange(CalculatorController.SCREEN_PROPERTY, result, "-" + result);
+                        result = "-" + result;
+                        lhs = new BigDecimal(result);
+                    }
+                }
+                else{
+                    String oldNumText = screen.toString();
+                    String newNumText = "";
+
+                    // toggle positive and negative input
+                    if(oldNumText.contains("-")){
+                        newNumText = oldNumText.substring(1);
+                    }
+                    else{
+                        newNumText = screen.insert(0, "-").toString();
+                    }
+
+                    // replace StringBuilder buffer
+                    screen.delete(0, screen.length());
+                    screen.append(newNumText);
+
+                    firePropertyChange(CalculatorController.SCREEN_PROPERTY, oldNumText, newNumText);
+                }
+
+                break;
+
+            case "btnAdd": case "btnSub": case "btnMul": case "btnDiv":
+
+                changeState(CalculatorState.OP_SELECTED);
+
+                if(tag.equals("btnAdd")) operator = Operator.ADD;
+                else if(tag.equals("btnSub")) operator = Operator.SUBTRACT;
+                else if(tag.equals("btnMul")) operator = Operator.MULTIPLY;
+                else if(tag.equals("btnDiv")) operator = Operator.DIVIDE;
+
+                if(screen.toString().contains(".")){
+                    tempDouble = Double.valueOf(screen.toString());
+                    lhs = new BigDecimal(tempDouble);
+                }
+                else{
+                    tempLong = Long.valueOf(screen.toString());
+                    lhs = new BigDecimal(tempLong);
+                }
+
+                break;
+
+            case "btnEqual":
+
+                if(screen.toString().contains(".")){
+                    tempDouble = Double.valueOf(screen.toString());
+                    rhs = new BigDecimal(tempDouble);
+                }
+                else{
+                    tempLong = Long.valueOf(screen.toString());
+                    rhs = new BigDecimal(tempLong);
+                }
+
+                changeState(CalculatorState.RESULT);
+
+                result = String.valueOf(calculateResult());
+
+                if( result.contains(".") && !(lhs.toString().contains(".")) &&
+                        !(rhs.toString().contains("."))){
+                    result = result.split("[.]", 0)[0];
+                }
+
+                clearScreen();
+
+                displayResult(result);
+
+                lhs = new BigDecimal(result);
+
+                break;
+
         }
 
     }
@@ -129,10 +258,6 @@ public class CalculatorModel extends AbstractModel {
 
         String oldText = screen.toString();
 
-        if(oldText.equals("0")){
-            screen.deleteCharAt(0);
-        }
-
         screen.append(digit);
         String newText = screen.toString();
 
@@ -142,8 +267,87 @@ public class CalculatorModel extends AbstractModel {
 
     }
 
+    private void clearScreen() {
+        String oldText = screen.toString();
+
+        if(state == CalculatorState.CLEAR){
+            // reset lhs, rhs, and operator
+            lhs = new BigDecimal(0);
+            rhs = new BigDecimal(0);
+            operator = null;
+
+            // clear the buffer
+            screen.delete(0, screen.length());
+
+            screen.append('0');
+
+            // place 0 as placeholder in screen
+            firePropertyChange(CalculatorController.SCREEN_PROPERTY, oldText, "0");
+        }
+        else{
+            screen.delete(0, screen.length());
+
+            firePropertyChange(CalculatorController.SCREEN_PROPERTY, oldText, "");
+        }
+
+    }
+
+    private double calculateResult(){
+        BigDecimal result = null;
+
+        switch(operator){
+
+            case ADD:
+
+                result = lhs.add(rhs);
+                result.setScale(2, RoundingMode.HALF_UP);
+
+
+
+                return result.doubleValue();
+
+            case SUBTRACT:
+
+                result = lhs.subtract(rhs);
+                result.setScale(2, RoundingMode.HALF_UP);
+
+                return result.doubleValue();
+
+            case MULTIPLY:
+
+                result = lhs.multiply(rhs);
+                result.setScale(2, RoundingMode.HALF_UP);
+
+                return result.doubleValue();
+
+            case DIVIDE:
+
+                result = lhs.divide(rhs, 2, RoundingMode.HALF_UP);
+                result.setScale(2, RoundingMode.HALF_UP);
+
+                return result.doubleValue();
+        }
+
+        return result.doubleValue();
+
+    }
+
+    private void displayResult(String result){
+
+        String oldText = screen.toString();
+
+        String newText = result;
+
+        firePropertyChange(CalculatorController.SCREEN_PROPERTY, oldText, newText);
+
+    }
+
     private enum CalculatorState {
         CLEAR, LHS, OP_SELECTED, RHS, RESULT, ERROR
+    }
+
+    private enum Operator {
+        ADD, SUBTRACT, MULTIPLY, DIVIDE
     }
 
 }
